44f2350352aeed12b70885ea226264faee2ff46f
0xnf <0xnf@wintech.com>
2023-09-12 15:50:33 +0900

    changed file names

---
+++ lib/src/dlogger_base.dart
@@ -0,0 +1,23 @@
+import 'package:dlogbasic/src/event_bus/events.dart';
+import 'package:dlogbasic/src/event_bus/event_bus.dart';
+import 'package:dlogbasic/src/targets/i_target.dart';
+import 'package:ilogger/ilogger.dart';
+
+abstract class DLoggerBase {
+  final String name;
+
+  final List<ITarget> targets;
+
+  const DLoggerBase({
+    required this.name,
+    this.targets = const [],
+  });
+
+  ///  Gets a value indicating whether logging is enabled for the specified level.
+  bool isEnabled(LogLevel level);
+
+  ///  Occurs when logger configuration changes.
+  Stream<LoggerReconfigured> onLoggerReconfigured() {
+    return on<LoggerReconfigured>();
+  }
+}
